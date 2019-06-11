19e60ece4996f2b124b7e343d6227b8b39904ab1
Lena Nigrell <nigrell@live.se>
2019-06-11 18:39:08 +0200

    work ongoing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_user_bookings
+
+  def set_user_bookings
+    if current_user
+      @my_bookings = current_user.bookings
+    else
+      @my_bookings = []
+    end
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+
+
+const bookings = document.getElementById("my-bookings");
+bookings.addEventListener("click", (event) => {
+  console.log("Hej");
+  console.log(event);
+  console.log(event.currentTarget);
+});
