078119368c717af59d58dacf89ebc3ee7e04439f
Lena Nigrell <nigrell@live.se>
2019-06-11 15:21:03 +0200

    view possibility to add new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,25 @@
 class BookingsController < ApplicationController
+  def new
+    @boat = Boat.find(params[:boat_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.boat = Boat.find(params[:boat_id])
+    # CHANGE User.last!!!!!
+    @booking.consumer = User.last
+    @booking.save
+    if @booking.save
+      redirect_to boat_booking_path(@booking.boat, @booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:consumer_id, :boat_id, :start_date, :end_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :boats, only: [:show, :new, :create]
+  resources :boats, only: [:show, :new, :create] do
+    resources :bookings, only: [:show, :new, :create]
+  end
+
 end
