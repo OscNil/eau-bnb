7fe9cef94b07f39907242e5905741e7cd3d9fa39
Lena Nigrell <nigrell@live.se>
2019-06-11 10:18:42 +0200

    creating boat

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,24 +1,29 @@
 class BoatsController < ApplicationController
 
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
   def new
     @boat = Boat.new
   end
 
   def create
-    @boat = Boat.new(prestaurant_params)
+    @boat = Boat.new(boat_params)
+    # CHANGE User.last!!!!!
+    @boat.owner = User.last
     @boat.save
-    # ==> boat_path does not exist yet
-    # if @boat.save
-    #   redirect_to boat_path(@boat)
-    # else
-    #   render :new
-    # end
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
   end
 
   private
 
-  def restaurant_params
-    params.require(:boat).permit(:owner_id, :day_price, :boat_name, :boat_pic_url,
+  def boat_params
+    params.require(:boat).permit(:owner_id, :day_price, :boat_name, :photo,
                                  :nbr_beds, :description)
   end
 end
