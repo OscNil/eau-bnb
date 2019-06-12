c63fd1d2cc9951d34c11600c458dec6a4adc3ae8
Dennis Persson <depersson_81@telia.com>
2019-06-12 15:48:30 +0200

    Owner can now see his/her bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get 'my-received-bookings', to: 'pages#my_received_bookings'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :boats, only: [:show, :new, :create, :index, :destroy] do
     resources :bookings, only: [:show, :new, :create]
